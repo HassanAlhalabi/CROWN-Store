ecbf71008b28f0b739cf69bfdfd1c9a81c449d86
Hassan Alhalabi <hassan@petabayte.com>
2022-12-15 10:54:38 +0100

    Add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.scss'
-import Directory from './components/directory'
-import { ICategory } from './models/category'
+import MainContainer from './components/layout/main-container'
+import Home from './pages/home'
+import Shop from './pages/shop'
 
-const categories: ICategory[] = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-];
 
 function App() {
 
   return (
     <div className="App">
-      <Directory categories={categories} />
+      <Routes>
+        <Route path='/' element={<MainContainer />}>
+          <Route index element={<Home />}/>
+          <Route path='/shop' element={<Shop />}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
---
+++ src/components/layout/main-container/index.tsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom"
+
+const MainContainer = () => {
+  return <Outlet />
+}
+
+export default MainContainer
